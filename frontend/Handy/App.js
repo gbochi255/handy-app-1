81751919eafa2eb2027c461a3809ec644e2a9610
Matt Crawford <mattcrawforduk@icloud.com>
2025-04-16 08:17:10 +0100

    register updates

--- frontend/Handy/App.js
+++ frontend/Handy/App.js
@@ -5,18 +5,21 @@ import { StyleSheet, Text, View } from "react-native";
 import SignIn from "./components/SignIn.js";
 import SignUp from "./components/SignUp.js";
 import CameraApp from "./components/CameraApp.js";
+import { UserProvider } from "./components/UserContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignIn">
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="CameraApp" component={CameraApp} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SignIn">
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="CameraApp" component={CameraApp} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 }
 
