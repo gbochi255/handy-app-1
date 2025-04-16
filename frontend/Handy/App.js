8ef62a895532fbd8c671741c80ab9c58aed9c291
Matt Crawford <mattcrawforduk@icloud.com>
2025-04-16 08:43:49 +0100

    added backdoor

--- frontend/Handy/App.js
+++ frontend/Handy/App.js
@@ -2,8 +2,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import SignIn from "./components/signIn.js";
+import SignIn from "./components/SignIn.js";
 import SignUp from "./components/SignUp.js";
+import CustomerHomepage from "./components/CustomerHomepage.js";
 import CameraApp from "./components/CameraApp.js";
 import { UserProvider } from "./components/UserContext";
 
@@ -17,6 +18,7 @@ export default function App() {
           <Stack.Screen name="SignIn" component={SignIn} />
           <Stack.Screen name="SignUp" component={SignUp} />
           <Stack.Screen name="CameraApp" component={CameraApp} />
+          <Stack.Screen name="CustomerHomepage" component={CustomerHomepage} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserProvider>
