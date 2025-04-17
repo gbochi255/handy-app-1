3cb5ddf3d5159d16ef918270c6b24308d6f0130a
gbochi255 <gbochi255@hotmail.com>
2025-04-17 10:24:54 +0100

    Updates on App

---
+++ frontend/Handy/components/UserContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+export const UserContext = createContext();
+export const UserProvider = ({ children }) => {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    postcode: "",
+    address: "",
+    region: "",
+    long: "",
+    lat: "",
+    bio: "",
+    photoUrl: "",
+    isProvider: false,
+    token: false,
+  });
+
+  return (
+    <UserContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
