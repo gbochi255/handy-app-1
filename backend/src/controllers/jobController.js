4b32605a89008e7264d6f1e1c04268baa7a76f91
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-04-17 17:30:39 +0100

    Refactored query for status and created_by - Still working on query for distance

--- backend/src/controllers/jobController.js
+++ backend/src/controllers/jobController.js
@@ -2,9 +2,9 @@ const { fetchJobs } = require("../models/jobModel")
 
 exports.getJobs = (request, response, next) => {
 
-    const { status, created_by } = request.query;
+    const { status, created_by, distance } = request.query;
 
-    fetchJobs(status, created_by)
+    fetchJobs(status, created_by, distance)
     .then((jobs) => {
         response.status(200).send(jobs);
     })
--- backend/src/models/jobModel.js
+++ backend/src/models/jobModel.js
@@ -1,18 +1,71 @@
 const db = require("../../db/connection")
 
-exports.fetchJobs = (status, created_by) => {
+exports.fetchJobs = (status, created_by, distance) => {
 
-    let queryStr = `SELECT * FROM jobs`
+    // let queryStr = `SELECT * FROM jobs j
+    // WHERE ($1::job_status IS NULL OR j.status = $1)
+    // AND ($2::integer IS NULL OR j.created_by = $2);`
 
-    if(status || created_by) queryStr += ` WHERE`
-    if(status) queryStr += ` status = '${status}'`
-    if(status && created_by) queryStr += ` AND`
-    if(created_by) queryStr += ` created_by = ${created_by}`
 
-console.log(queryStr)
 
-    return db.query(queryStr)
-    .then(({ rows }) => {
-        return { jobs: rows }
-    })
+    let queryStrDistance = `SELECT
+    j.job_id,
+    j.summary,
+    j.category,
+    j.status,
+    ST_X(j.location) AS job_latitude,
+    ST_Y(j.location) AS job_longitude,
+    ROUND(
+        CAST(ST_Distance(
+            j.location::geography,
+            (SELECT location::geography FROM users WHERE user_id = $3)
+        ) / 1609.34 AS numeric), 2
+    ) AS distance_miles
+FROM jobs j
+WHERE
+    j.location IS NOT NULL
+    AND ($1::job_status IS NULL OR j.status = $1)
+    AND ($2::integer IS NULL OR j.created_by = $2)
+    AND (
+    $4::float IS NULL OR ST_DWithin(
+        j.location::geography,
+        (SELECT location::geography FROM users WHERE user_id = $3),
+        $4 * 1609.34 -- 50 miles in meters
+))
+ORDER BY distance_miles;`
+
+    let queryVariables = [status || null, created_by || null, user_id || null, distance || null]
+
+    //         queryStr = `SELECT
+    //     j.job_id,
+    //     j.summary,
+    //     j.category,
+    //     j.status,
+    //     ST_X(j.location) AS job_latitude,
+    //     ST_Y(j.location) AS job_longitude,
+    //     ROUND(
+    //         CAST(ST_Distance(
+    //             j.location::geography,
+    //             (SELECT location::geography FROM users WHERE user_id = 1)
+    //         ) / 1609.34 AS numeric), 2
+    //     ) AS distance_miles
+    // FROM jobs j
+    // WHERE
+    //     j.location IS NOT NULL
+    //     AND ST_DWithin(
+    //         j.location::geography,
+    //         (SELECT location::geography FROM users WHERE user_id = 1),
+    //         50 * 1609.34 -- 50 miles in meters
+    //     )
+    // ORDER BY distance_miles;`
+    // //     }
+
+
+    console.log(queryStrDistance)
+    console.log(queryVariables)
+
+    return db.query(queryStrDistance, [...queryVariables])
+        .then(({ rows }) => {
+            return { jobs: rows }
+        })
 }
