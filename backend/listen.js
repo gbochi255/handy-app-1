b84532ac3a9f4a341ebc6250bb3764630c471338
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-04-22 15:23:03 +0100

    Changed listen file to console.log the environment variable

--- backend/listen.js
+++ backend/listen.js
@@ -1,4 +1,5 @@
 const app = require("./app.js")
 const { PORT = 3000 } = process.env
+const ENV = process.env.NODE_ENV
 
-app.listen(PORT, () => console.log(`server running on ${PORT}`));
+app.listen(PORT, () => console.log(`server running on ${ENV}`));
