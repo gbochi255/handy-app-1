de14a6f8c1bd1bc882e753068d75d2b3387a8e73
gbochi255 <126166418+gbochi255@users.noreply.github.com>
2025-07-18 13:05:13 +0100

    updated README

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,7 +1,7 @@
 const ENV = process.env.NODE_ENV || "test";
 // const ENV = process.env.NODE_ENV || "development";
 require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
-const { Pool,types } = require("pg");
+const { Pool, types } = require("pg");
 
 // console.log("ENV is :", ENV);
 const config = {
@@ -10,6 +10,8 @@ const config = {
 
 // Override DATE type parser (OID 1082)
 types.setTypeParser(1082, (value) => value); // Return raw string (YYYY-MM-DD)
+types.setTypeParser(1114, (value) => value); // TIMESTAMP
+types.setTypeParser(1114, (value) => value); //TIMESTAMPTZ
 
 const PORT = process.env.PORT || 3000;
 
